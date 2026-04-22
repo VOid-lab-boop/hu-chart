8843068efce7a0ebdfb80fb7d4c6bbb85ff0f149
VOid-lab-boop <yamanakour5@gmail.com>
2026-04-22 21:07:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "/hu-chart/",
   server: {
     host: "::",
     port: 8080,
